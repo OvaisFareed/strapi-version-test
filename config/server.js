2a33f0aa560000511a999c56a78aa0bc8d4ebde3
ovais-mtfxgroup <ovais.siddiqui@mtfxgroup.com>
2024-02-06 03:18:46 +0500

    setup project files

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ff0a62b79f74ce973734e4f451c3e684'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f12c7314-63c1-422f-bc00-0bea7ca3d3d3'
+};
